21643ce74076aa8def93ca1243624d7d450e3071
luongthuan-lv <luongthuan2k@gmail.com>
2019-09-29 01:23:50 +0700

    Hello

--- app/src/main/java/vn/edu/contenprovider/MainActivity.java
+++ app/src/main/java/vn/edu/contenprovider/MainActivity.java
@@ -1,38 +1,51 @@
 package vn.edu.contenprovider;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.loader.content.CursorLoader;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Picture;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CallLog;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     public TextView textView;
     private ListView lvList;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvList = findViewById(R.id.lvList);
+        imageView = findViewById(R.id.imageView);
 
     }
 
@@ -70,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         String[] projection = new String[]{
                 CallLog.Calls.DATE,
                 CallLog.Calls.NUMBER,
+                CallLog.Calls.CACHED_NAME,
                 CallLog.Calls.DURATION
         };
         ArrayList<String> list = new ArrayList<>();
@@ -81,10 +95,14 @@ public class MainActivity extends AppCompatActivity {
         c.moveToFirst();
         String s = "";
         while (c.isAfterLast() == false) {
-            for (int i = 0; i < c.getColumnCount(); i++) {
-                s += c.getString(i) + " - ";
-            }
-            s += "\n";
+
+            Long date = c.getLong(c.getColumnIndex(CallLog.Calls.DATE));
+            Timestamp now=new Timestamp(date);
+            Date date1=new Date(now.getTime());
+            String number = c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));
+            String name = c.getString(c.getColumnIndex(CallLog.Calls.CACHED_NAME));
+            String duration = c.getString(c.getColumnIndex(CallLog.Calls.DURATION));
+            s = date1 + " - " + number + " - " + name + " - " + " - " + duration + "\n";
             c.moveToNext();
             list.add(s);
         }
@@ -96,22 +114,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void run() {
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;//hoặc sử dụng đường dẫn ContactsContacts.CONTENT_URI;
-//        String[] projection={
-//                ContactsContract.CommonDataKinds.Phone._ID,
-//        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-//                ContactsContract.CommonDataKinds.Phone.NUMBER
-//        };
+        String[] projection = {
+                ContactsContract.CommonDataKinds.Phone._ID,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER
+        };
         ArrayList<String> list = new ArrayList<>();
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        String data = "";
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+
         if (cursor != null) {
             if (cursor.getCount() > 0) {
                 cursor.moveToFirst();
                 while (cursor.isAfterLast() == false) {
-                    //String id = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
+                    String data = "";
+                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
+
+
                     String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+
                     String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    data =  name + "-" + number + "\n";
+
+                    data += id + " - " + name + " - " + number;
 
                     cursor.moveToNext();
                     list.add(data);
@@ -131,37 +155,101 @@ public class MainActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 887);
         } else {
-            media();
+            getRealPathByUri(MainActivity.this,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         }
     }
 
-    private void media() {
-        String[] projection = {
-                MediaStore.MediaColumns.DISPLAY_NAME,
-                MediaStore.MediaColumns.DATE_ADDED,
-                MediaStore.MediaColumns.MIME_TYPE
-        };
-        ArrayList<String> list = new ArrayList<>();
-        CursorLoader loader = new CursorLoader
-                (this, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                        projection, null, null, null);
-        Cursor c = loader.loadInBackground();
-        c.moveToFirst();
-        String media = "";
-        while (!c.isAfterLast()) {
-            for (int i = 0; i < c.getColumnCount(); i++) {
-                media += c.getString(i) + " - ";
+//    private void media() {
+//
+//        ContentResolver contentResolver = MainActivity.this.getContentResolver();
+//        String[] projection = {
+//                MediaStore.Images.Media._ID
+//        };
+//        Uri ext_uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//        String where = "(" + MediaStore.Images.Media.MIME_TYPE + "=? or " + MediaStore.Images.Media.MIME_TYPE + "=?)" + " and " + MediaStore.Images.Media.SIZE + ">=?";
+//        ArrayList<String> list = new ArrayList<>();
+//
+//        Cursor c = MediaStore.Images.Media.query(
+//                contentResolver,
+//                ext_uri,
+//                projection,
+//                where,
+//                new String[]{"image/jpeg", "image/png", "102400"},
+//                MediaStore.Images.Media.DATE_MODIFIED + " desc"
+//        );
+//        //int columnIndex = c.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+//        int i = 0;
+//        while (c.moveToNext() && i < c.getCount()) {
+//            String picture=Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null+"").toString();
+//            Log.e("url",picture);
+//            File file=new File(picture);
+//            if(file.exists()) {
+//                Bitmap myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+//
+//                this.imageView.setImageBitmap(myBitmap);
+//
+//            }
+//        }
+//
+//        c.close()
+//         }
+
+//    public void image(){
+//        ContentResolver contentResolver = MainActivity.this.getContentResolver();
+//        String[] projection = {
+//                MediaStore.Images.Media._ID
+//        };
+//        Uri ext_uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//        String where = "(" + MediaStore.Images.Media.MIME_TYPE
+//                + "=? or " + MediaStore.Images.Media.MIME_TYPE + "=?)"
+//                + " and " + MediaStore.Images.Media.SIZE + ">=?";
+//        ArrayList<Picture> listpic = new ArrayList<>();
+//        Cursor c = MediaStore.Images.Media.query(
+//                contentResolver,
+//                ext_uri,
+//                projection,
+//                where,
+//                new String[] { "image/jpeg", "image/png", "102400" },
+//                MediaStore.Images.Media.DATE_MODIFIED + " desc");
+//        int columnIndex = c.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+//        int i = 0;
+//        while (c.moveToNext() && i < c.getCount()) {
+//            long origId = c.getLong(columnIndex);
+//            Picture picture = new Picture();
+//            picture.uri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, origId + "").toString();
+//            listpic.add(picture);
+//            i++;
+//        }
+//        c.close();
+//        return null;
+//    }
+
+    public String getRealPathByUri(Context context, Uri uri) {
+        if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+            ContentResolver resolver = context.getContentResolver();
+            String[] proj = new String[]{MediaStore.Images.Media.DATA};
+            Cursor cursor = MediaStore.Images.Media.query(resolver, uri, proj);
+            String realPath = null;
+            if (cursor != null) {
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.getCount() > 0 && cursor.moveToFirst()) {
+                    realPath = cursor.getString(columnIndex);
+                    File file=new File(realPath);
+            if(file.exists()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+
+                MainActivity.this.imageView.setImageBitmap(myBitmap);
+
             }
-            media += "\n";
-            c.moveToNext();
-            list.add(media);
-        }
-        c.close();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, list);
-        lvList.setAdapter(adapter);
+                }
+
+            }
+            return realPath;
 
     }
-
 
 }
 
